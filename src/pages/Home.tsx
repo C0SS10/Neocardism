37c77b647854dd2de96853b3c3ca27fb6c86043a
Esteban <estebancg12@hotmail.com>
2025-06-17 15:46:22 -0500

    Refactor App component and add Home page ↪️

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="w-full h-screen flex items-center justify-center bg-slate-200 font-sans">
+      <h1 className="text-emerald-800 text-4xl font-bold">Neucardism</h1>
+    </section>
+  );
+};
+
+export default Home;
